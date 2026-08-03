4d9ca51504e82212e3cc7bb65521395d20f06f1b
Navneet Singh <nav9027388401singh@gmail.com>
2026-08-03 21:27:50 +0530

    Single Threaded Server Communication based on prebuilt prompts

--- SingleThreaded/Client.java
+++ SingleThreaded/Client.java
@@ -1,12 +1,32 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.Socket;
+import java.util.Scanner;
 
 public class Client {
     public static void main(String args[]) throws Exception {
         Socket socket = new Socket("localhost", 8010);
+
         BufferedReader fromServer = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-        System.out.println("Server says: " + fromServer.readLine());
+        PrintWriter toServer = new PrintWriter(socket.getOutputStream(), true);
+        Scanner keyboard = new Scanner(System.in);
+
+        System.out.println("Connected to the server. Type messages (type 'bye' to quit):");
+
+        while (true) {
+            System.out.print("You: ");
+            String userInput = keyboard.nextLine();
+
+            toServer.println(userInput);
+            String reply = fromServer.readLine();
+            System.out.println("Server: " + reply);
+
+            if (userInput.equalsIgnoreCase("bye")) {
+                break;
+            }
+        }
+
         socket.close();
     }
 }
--- SingleThreaded/Server.java
+++ SingleThreaded/Server.java
@@ -1,3 +1,5 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
@@ -11,10 +13,35 @@ public class Server {
             Socket clientSocket = serverSocket.accept();
             System.out.println("Connected: " + clientSocket.getRemoteSocketAddress());
 
+            BufferedReader fromClient = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
             PrintWriter toClient = new PrintWriter(clientSocket.getOutputStream(), true);
+            String message;
+            while ((message = fromClient.readLine()) != null) {
+                System.out.println("Client says: " + message);
+                String reply = respondTo(message);
+                toClient.println(reply);
+                if (message.equalsIgnoreCase("bye")) {
+                    break;
+                }
+            }
             toClient.println("Hello from the Server!");
 
             clientSocket.close();
+            System.out.println("Client disconnected. Waiting for next client...");
+        }
+    }
+
+    private static String respondTo(String message) {
+        String msg = message.trim().toLowerCase();
+
+        if (msg.equals("hello") || msg.equals("hi") || msg.equals("hey")) {
+            return "Hello there! How can I help you";
+        } else if (msg.equals("time")) {
+            return "Server time is: " + java.time.LocalDateTime.now().toString();
+        } else if (msg.equals("bye")) {
+            return "Goodbye! Have a great day!";
+        } else {
+            return "I don't understand: \"" + message + "\". Try 'hello', 'time', or 'bye'";
         }
     }
 }
